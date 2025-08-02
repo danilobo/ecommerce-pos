f4ecb186df1bacfce5b0d0296d91002643656a59
Daniel Lobo <>
2025-08-02 13:59:53 -0300

    Add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+// src/components/Layout/Header.tsx
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1>Portal de Produtos</h1>
+    </header>
+  );
+};
+
+export default Header;
